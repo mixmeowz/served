1a3d5a561f58ae7332062870ed76cbff0253c6cc
meowmix <35702702+mixmeowz@users.noreply.github.com>
2018-02-12 13:09:56 -0700

    Add files via upload

---
+++ R/served-data.R
@@ -0,0 +1,19 @@
+#' Person Served
+#'
+#' Data from a QTL experiment on gravitropism in
+#' Arabidopsis, with data on 162 recombinant inbred lines (Ler x
+#' Cvi). The outcome is the root tip angle (in degrees) at two-minute
+#' increments over eight hours.
+#'
+#' @docType data
+#'
+#' @usage data(served)
+#'
+#'
+#' @keywords datasets
+#'
+#'
+#'
+#' @examples
+#' data(served)
+"served"
